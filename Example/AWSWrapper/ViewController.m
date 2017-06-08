a6ffdc3ca0ca658dacf4a62600ddde47285390fd
Stan Liu <lyc2345@gmail.com>
2017-06-08 16:43:34 +0800

    change ui

--- Example/AWSWrapper/ViewController.m
+++ Example/AWSWrapper/ViewController.m
@@ -10,20 +10,22 @@
 #import "DynamoDBVC.h"
 @import AWSWrapper;
 
-@interface ViewController () <UITextFieldDelegate>
+@interface ViewController () <UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource> {
+  
+  __weak IBOutlet UITableView *_tableView;
+}
 
 @property (weak, nonatomic) IBOutlet UILabel *identityLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
-
 @property (weak, nonatomic) IBOutlet UITextField *nameTF;
-
-@property (weak, nonatomic) IBOutlet UITextView *console;
 @property (weak, nonatomic) IBOutlet UITextView *userHistoryConsole;
 @property (weak, nonatomic) IBOutlet UILabel *checkLoginLabel;
 
-@property (strong, nonatomic) NSArray *textfields;
+@property NSDictionary *localBookmark;
+@property NSDictionary *remoteBookmark;
+@property NSDictionary *localRecentVisitItems;
+@property NSDictionary *remoteRecentVisitItems;
 
 @end
 
@@ -42,13 +44,20 @@
 	
 	[SyncManager shared];
 	
-	self.textfields = @[self.nameTF];
-	
-	for (UITextField *tf in self.textfields) {
-		tf.text = @"a";
-	}
-	
 	[self refreshLoginStatusThroughNotification];
+  
+  _tableView.delegate = self;
+  _tableView.dataSource = self;
+  
+  [_tableView registerClass: [UITableViewCell class] forCellReuseIdentifier: @"cell"];
+  
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear: animated];
+  
+  [self reloadBookmarks];
+  [self reloadRecentlyVisit];
 }
 
 -(void)refreshLoginStatusThroughNotification {
@@ -68,24 +77,6 @@
 	
 	_checkLoginLabel.text = [NSString stringWithFormat:@"status offline: %@, remote: %@", ([LoginManager shared].isLogin) ? @"YES" : @"NO" , ([LoginManager shared].isAWSLogin) ? @"YES" : @"NO"];
 }
-
--(void)print:(NSArray *)bookmarks {
-	
-	NSMutableString *log = [NSMutableString string];
-	
-	[bookmarks enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-		
-		NSDictionary *dict = bookmarks[idx];
-		[log appendString: @"{ \n"];
-		[log appendString: [NSString stringWithFormat: @"  \"comicName\": \"%@\",", dict[@"comicName"]]];
-		[log appendString: [NSString stringWithFormat: @"  \"author\": \"%@\",", dict[@"author"]]];
-		[log appendString: [NSString stringWithFormat: @"  \"url\": \"%@\",", dict[@"url"]]];
-		[log appendString: @"\n }, \n"];
-	}];
-	
-	NSLog(@"bookmarks: %@", log);
-}
-
 
 - (IBAction)log:(id)sender {
 	
@@ -120,7 +111,7 @@
 - (IBAction)load:(id)sender {
 	
 	NSDictionary *userBookmark = [[BookmarkManager new] getOfflineRecordOfIdentity: [LoginManager shared].awsIdentityId type: RecordTypeBookmark];
-	self.console.text = [NSString stringWithFormat:@"%@", userBookmark];
+	//self.console.text = [NSString stringWithFormat:@"%@", userBookmark];
 	
 	
 	NSArray *userList = [[NSUserDefaults standardUserDefaults] arrayForKey: @"__USER_LIST"];
@@ -129,11 +120,8 @@
 	
 	[self refreshLoginStatusThroughNotification];
   
-  [[BookmarkManager new] pullType: RecordTypeBookmark user: [LoginManager shared].awsIdentityId completion:^(NSDictionary *item, NSError *error) {
-    
-    NSLog(@"%@", item);
-    
-  }];
+  [self reloadBookmarks];
+  [self reloadRecentlyVisit];
 }
 
 
@@ -153,34 +141,8 @@
       NSLog(@"error: %@", error);
       return;
     }
+    [self reloadBookmarks];
   }];
-  /*
-   NSDictionary *record = [[BookmarkManager new] getOfflineRecordOfIdentity: [LoginManager shared].awsIdentityId type: RecordTypeBookmark];
-   
-   Bookmark *bk = [Bookmark new];
-   bk._id = [LoginManager shared].awsIdentityId;
-   bk._userId = [LoginManager shared].awsIdentityId;
-   bk._dicts = record[@"_dicts"];
-   bk._remoteHash = record[@"_remoteHash"];
-   bk._commitId = record[@"_commitId"];
-   
-   [[BookmarkManager new] mergePushWithRecord: bk type: RecordTypeBookmark completion:^(NSError *error) {
-   
-   if (error) {
-			NSLog(@"error: %@", error);
-			return;
-   }
-   }];*/
-}
-
-- (IBAction)delete:(id)sender {
-	
-	NSDictionary *bookmarkToBeDeleted = @{@"comicName": self.nameTF.text, @"author": self.nameTF.text, @"url": self.nameTF.text};
-
-	if ([LoginManager shared].isLogin) {
-		
-		[[BookmarkManager new] deleteOffline: bookmarkToBeDeleted type: RecordTypeBookmark ofIdentity: [LoginManager shared].awsIdentityId];
-	}
 }
 
 - (IBAction)saveRecentlyVisit:(id)sender {
@@ -193,74 +155,78 @@
 		[[BookmarkManager new] addOffline: recentlyVisit type: RecordTypeRecentlyVisit ofIdentity: [LoginManager shared].awsIdentityId];
 		
 	}
-  /*
-	RecentVisit *rv = [RecentVisit new];
-	rv._id = [LoginManager shared].awsIdentityId;
-	rv._userId = [LoginManager shared].awsIdentityId;
-	NSDictionary *record = [[BookmarkManager new] getOfflineRecordOfIdentity: [LoginManager shared].awsIdentityId type: RecordTypeRecentlyVisit];
-	
-	rv._dicts = record[@"_dicts"];
-	rv._remoteHash = record[@"_remoteHash"];
-	rv._commitId = record[@"_commitId"];
-	
-	[[BookmarkManager new] mergePushWithRecord: rv type: RecordTypeRecentlyVisit completion:^(NSError *error) {
-		
-		if (error) {
-			NSLog(@"error: %@", error);
-			return;
-		}
-	}];*/
   [[BookmarkManager new] mergePushType: RecordTypeRecentlyVisit userId:[LoginManager shared].awsIdentityId completion:^(NSError *error) {
     
     if (error) {
       NSLog(@"error: %@", error);
       return;
     }
+    [self reloadRecentlyVisit];
   }];
-}
-
-
-- (IBAction)clear:(id)sender {
-
-	for (UITextField *tf in self.textfields) {
-
-			[tf setText: @""];
-	}
 }
 
 - (IBAction)syncRemote:(id)sender {
 	
-	[[SyncManager shared] startLoginFlow];
+	//[[SyncManager shared] startLoginFlow];
+  NSString *userId = [LoginManager shared].awsIdentityId;
+  [[BookmarkManager new] mergePushType: RecordTypeBookmark userId: userId completion:^(NSError *error) {
+    
+    dispatch_sync(dispatch_get_main_queue(), ^{
+      [self reloadBookmarks];
+    });
+  }];
+  [[BookmarkManager new] mergePushType: RecordTypeRecentlyVisit userId: userId completion:^(NSError *error) {
+    
+    dispatch_sync(dispatch_get_main_queue(), ^{
+      [self reloadRecentlyVisit];
+    });
+  }];
 }
 
-- (IBAction)browseDataOfUser:(id)sender {
-	
-	DynamoDBVC *dbVC = [self.storyboard instantiateViewControllerWithIdentifier: @"DynamoDBVC"];
-	[self.navigationController pushViewController: dbVC animated: true];
+
+-(void)reloadBookmarks {
+  
+  BookmarkManager *bookmarkManager = [BookmarkManager new];
+  LoginManager *loginManager = [LoginManager shared];
+  NSString *userId = loginManager.awsIdentityId != nil ? loginManager.awsIdentityId : loginManager.offlineIdentity;
+  NSDictionary *localBookmarkRecord = [bookmarkManager getOfflineRecordOfIdentity: userId type: RecordTypeBookmark];
+  
+  self.localBookmark = localBookmarkRecord;
+  [_tableView reloadData];
+  
+  if ([LoginManager shared].awsIdentityId) {
+    [bookmarkManager pullType: RecordTypeBookmark user: loginManager.awsIdentityId completion:^(NSDictionary *item, NSError *error) {
+      
+      dispatch_async(dispatch_get_main_queue(), ^{
+        
+        self.remoteBookmark = item;
+        [_tableView reloadSections: [NSIndexSet indexSetWithIndex: 1] withRowAnimation: UITableViewRowAnimationNone];
+      });
+    }];
+  }
 }
 
-- (IBAction)logRecentData:(id)sender {
-	
-  [[BookmarkManager new] pullType: RecordTypeRecentlyVisit user: [LoginManager shared].awsIdentityId completion:^(NSDictionary *item, NSError *error) {
-		
-		NSLog(@"recent data: %@", item);
-		dispatch_async(dispatch_get_main_queue(), ^{
-			self.console.text = [NSString stringWithFormat:@"%@", item];
-		});
-	}];
-}
-
-- (IBAction)logBookmark:(id)sender {
-	
-  [[BookmarkManager new] pullType: RecordTypeBookmark user: [LoginManager shared].awsIdentityId completion:^(NSDictionary *item, NSError *error) {
-
-		
-		NSLog(@"recent data: %@", item);
-		dispatch_async(dispatch_get_main_queue(), ^{
-				self.console.text = [NSString stringWithFormat:@"%@", item];
-		});
-		
-	}];
+-(void)reloadRecentlyVisit {
+  
+  BookmarkManager *bookmarkManager = [BookmarkManager new];
+  LoginManager *loginManager = [LoginManager shared];
+  NSString *userId = loginManager.awsIdentityId != nil ? loginManager.awsIdentityId : loginManager.offlineIdentity;
+  NSDictionary *localRecentlyVisit = [bookmarkManager getOfflineRecordOfIdentity: userId type: RecordTypeRecentlyVisit];
+  
+  self.localRecentVisitItems = localRecentlyVisit;
+  [_tableView reloadData];
+  
+  if ([LoginManager shared].awsIdentityId) {
+    
+    [bookmarkManager pullType: RecordTypeRecentlyVisit user: loginManager.awsIdentityId completion:^(NSDictionary *item, NSError *error) {
+      
+      dispatch_async(dispatch_get_main_queue(), ^{
+        
+        self.remoteRecentVisitItems = item;
+        [_tableView reloadSections: [NSIndexSet indexSetWithIndex: 3] withRowAnimation: UITableViewRowAnimationNone];
+      });
+    }];
+  }
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -268,5 +234,109 @@
 	[textField resignFirstResponder];
 	return true;
 }
+
+// MARK: TableViewDelegate
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  
+  return 4;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  
+  if (section == 0) {
+    return [(NSArray *)self.localBookmark[@"_dicts"] count];
+  } else if (section == 1) {
+    return [(NSArray *)self.remoteBookmark[@"_dicts"] count];
+  } else if (section == 2) {
+    return [(NSArray *)self.localRecentVisitItems[@"_dicts"] count];
+  } else if (section == 3){
+    return [(NSArray *)self.remoteRecentVisitItems[@"_dicts"] count];
+  }
+  return 0;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  
+  if (section == 0) {
+    return [NSString stringWithFormat:@"Local B count %lu- %@", (unsigned long)[(NSArray *)self.localBookmark[@"_dicts"] count], [self.localBookmark[@"_commitId"] substringWithRange: NSMakeRange(((NSString *)self.localBookmark[@"_commitId"]).length - 10, 10)]];
+  } else if (section == 1) {
+    return [NSString stringWithFormat:@"Remote B count %lu- %@", (unsigned long)[(NSArray *)self.remoteBookmark[@"_dicts"] count], [self.remoteBookmark[@"_commitId"] substringWithRange: NSMakeRange(((NSString *)self.remoteBookmark[@"_commitId"]).length - 10, 10)]];
+    
+  } else if (section == 2) {
+    return [NSString stringWithFormat:@"Local R count %lu- %@", (unsigned long)[(NSArray *)self.localRecentVisitItems[@"_dicts"] count], [self.localRecentVisitItems[@"_commitId"] substringWithRange: NSMakeRange(((NSString *)self.localRecentVisitItems[@"_commitId"]).length - 10, 10)]];
+  } else if (section == 3) {
+    return [NSString stringWithFormat:@"Remote R count %lu- %@", (unsigned long)[(NSArray *)self.remoteRecentVisitItems[@"_dicts"] count], [self.remoteRecentVisitItems[@"_commitId"] substringWithRange: NSMakeRange(((NSString *)self.remoteRecentVisitItems[@"_commitId"]).length - 10, 10)]];
+  }
+  return @"";
+}
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+  return indexPath.section % 2 == 0 ? YES : NO;
+}
+
+-(NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+  
+  UITableViewRowAction *delete = [UITableViewRowAction rowActionWithStyle: UITableViewRowActionStyleDefault title: @"Delete" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+    
+    BookmarkManager *bookmarkManager = [BookmarkManager new];
+    
+    if (indexPath.section == 0) {
+      
+      [tableView beginUpdates];
+      self.localBookmark = [bookmarkManager deleteOffline: [DSWrapper arrayFromDict: self.localBookmark[@"_dicts"]][indexPath.row] type: RecordTypeBookmark ofIdentity: self.localBookmark[@"_identity"]];
+      [tableView deleteRowsAtIndexPaths: @[indexPath] withRowAnimation: UITableViewRowAnimationLeft];
+      [tableView reloadSectionIndexTitles];
+      [tableView endUpdates];
+      
+    } else if (indexPath.section == 2) {
+      
+      [tableView beginUpdates];
+      self.localRecentVisitItems = [bookmarkManager deleteOffline: [DSWrapper arrayFromDict: self.localRecentVisitItems[@"_dicts"]][indexPath.row] type: RecordTypeRecentlyVisit ofIdentity: self.localRecentVisitItems[@"_identity"]];
+      [tableView deleteRowsAtIndexPaths: @[indexPath] withRowAnimation: UITableViewRowAnimationLeft];
+      [tableView reloadSectionIndexTitles];
+      [tableView endUpdates];
+    }
+    
+  }];
+  
+  return @[delete];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"cell"];
+  
+  if (!cell) {
+    cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: @"cell"];
+  }
+  
+  if (indexPath.section == 0) {
+    
+    NSArray *bks = [DSWrapper arrayFromDict: self.localBookmark[@"_dicts"]];
+    NSDictionary *bk = bks[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat: @"%@, %@, %@", bk[@"comicName"], bk[@"author"], bk[@"url"]];
+    
+  } else if (indexPath.section == 1) {
+    
+    NSArray *comics = [DSWrapper arrayFromDict: self.remoteBookmark[@"_dicts"]];
+    NSDictionary *bk = comics[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat: @"%@, %@, %@", bk[@"comicName"], bk[@"author"], bk[@"url"]];
+    
+  } else if (indexPath.section == 2)  {
+    
+    NSArray *bks = [DSWrapper arrayFromDict: self.localRecentVisitItems[@"_dicts"]];
+    NSDictionary *bk = bks[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat: @"%@, %@, %@", bk[@"comicName"], bk[@"author"], bk[@"url"]];
+    
+  } else if (indexPath.section == 3)  {
+    
+    NSArray *comics = [DSWrapper arrayFromDict: self.remoteRecentVisitItems[@"_dicts"]];
+    NSDictionary *bk = comics[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat: @"%@, %@, %@", bk[@"comicName"], bk[@"author"], bk[@"url"]];
+  }
+  return cell;
+}
+
+
 
 @end
